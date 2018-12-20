ef85935c868f36db2193b8b484c69fdd674c8297
Trainee GSS <trainee@kgisl.com>
2018-12-20 10:41:30 +0530

    Commit For Pwa  Pwa Commit Application  link - https://www.smashingmagazine.com/2018/09/pwa-angular-6/

---
+++ src/app/angular-service.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export  interface  Item{
+  name:  string;
+  description:  string;
+  url:  string;
+  html:  string;
+  markdown:  string;
+  }
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AngularServiceService {
+
+   private url :string ='https://www.techiediaries.com/api/data.json';
+
+  constructor(private http:HttpClient) { }
+
+
+  fetch():  Observable<Item[]>{
+    return <Observable<Item[]>> this.http.get(this.url);
+    }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item, AngularServiceService } from './angular-service.service';
+import { MethodCall } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
+  ngOnInit(): void {
+    this.mehtodCall();
+  }
   title = 'AngularTestProject';
+
+
+  items:  Array<Item>;
+   constructor(  private servive:AngularServiceService){
+      
+   }
+    mehtodCall()
+    {
+       this.servive.fetch().subscribe((data:Array<Item>)=>
+       {
+          this.items=data;
+       },
+       (err)=>{
+          
+       }
+       );
+      }
+
+  
 }
