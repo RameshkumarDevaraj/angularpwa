f61a3047b9a063f26fad54694a2f4f974e0040e2
Trainee GSS <trainee@kgisl.com>
2018-12-20 16:48:08 +0530

    stage file

--- src/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static('./dist/pwademo'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/pwademo/index.html'));
-});
-app.listen(process.env.PORT || 8080);
