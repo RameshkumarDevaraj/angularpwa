dad495c083d39f9fdb65c804d8e7f2bedb8cbf73
RameshkumarDevaraj <46015049+RameshkumarDevaraj@users.noreply.github.com>
2018-12-21 16:22:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/angularpwa'));
+app.use(express.static('./dist/AngularTestProject'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/angularpwa/index.html'));
+  res.sendFile(path.join('./dist/AngularTestProject/index.html'));
 });
 app.listen(process.env.PORT || 8080);
